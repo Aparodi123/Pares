2f6ff6b7b67628c5f51877bdfabe1607a55c1f0e
aparodi <aparodi@frsn.utn.edu.ar>
2024-04-18 15:51:55 -0300

    Clase de ciclos

---
+++ pares.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+int num =;
+cout<<"ingrese un numero: ";
+        cin>>num;
+
+
+for(int i = 1; i <= num; i++){
+
+
+
+
+        if(i%2 == 0 ){
+
+
+            cout<< i<< endl;
+
+        }
+
+}
+
+
+
+
+
+    return 0;}
